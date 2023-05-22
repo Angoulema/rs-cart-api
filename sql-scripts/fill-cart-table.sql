40fec9f6b5747fd59c76f42e22c565634bc888db
Evgeniya Labokha <70232156+Angoulema@users.noreply.github.com>
2023-05-22 13:21:10 +0300

    feat: task 8.2 add scripts

---
+++ sql-scripts/fill-cart-table.sql
@@ -0,0 +1,4 @@
+insert into cart (status, created_at, updated_at) values ('OPEN', '2023-05-22', '2023-05-22');
+insert into cart (status, created_at, updated_at) values ('OPEN', '2023-05-22', '2023-05-22');
+insert into cart (status, created_at, updated_at) values ('OPEN', '2023-05-22', '2023-05-22');
+insert into cart (status, created_at, updated_at) values ('ORDERED', '2023-05-21', '2023-05-22');
---
+++ sql-scripts/fill-cart_items-table.sql
@@ -0,0 +1,4 @@
+insert into cart_items (count_, cart_id) values (2, '0ed715da-c8b5-4367-8afc-30d76f752e37');
+insert into cart_items (count_, cart_id) values (1, '13e2ee47-be95-4601-9ca7-c3cd367890b5');
+insert into cart_items (count_, cart_id) values (5, '555cc98d-f1ec-4bd4-acab-5b91d9bf3592');
+insert into cart_items (count_, cart_id) values (1, '1a73b800-fd6a-42f0-9e97-c5b7c111c0e5');
